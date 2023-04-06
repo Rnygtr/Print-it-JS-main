0ff747d78e5c8f80a89e5fc309727ee9c2368360
Rnygtr <116190095+Rnygtr@users.noreply.github.com>
2023-04-06 23:46:24 +0100

    fin

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,62 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+
+// bullet points
+
+const points = document.querySelector(".dots");
+
+for (let i = 0; i < slides.length; i++) {
+  const dot = document.createElement("div");
+  points.appendChild(dot);
+  dot.classList.add("dot");
+}
+
+let position = 0;
+let selectedDot = recuperationPosition();
+selectedDot.classList.add("dot_selected");
+
+// Ajout des évènements sur les flèches
+
+const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
+
+arrowRight.addEventListener("click", function () {
+  clicSurFleche(1);
+});
+arrowLeft.addEventListener("click", function () {
+  clicSurFleche(-1);
+});
+
+// FONCTIONS
+
+function recuperationPosition() {
+  return document.querySelector(`.dots .dot:nth-child(${position + 1}`);
+}
+
+function clicSurFleche(direction) {
+  position = position + direction;
+  testPosition();
+  changementBulletPoint();
+  changementImage();
+}
+
+function testPosition() {
+  if (position >= slides.length) {
+    position = 0;
+  } else if (position < 0) {
+    position = slides.length - 1;
+  }
+}
+
+function changementBulletPoint() {
+  selectedDot.classList.remove("dot_selected");
+  selectedDot = recuperationPosition();
+  selectedDot.classList.add("dot_selected");
+}
+
+function changementImage() {
+  document.querySelector(".banner-img").src =
+    "./assets/images/slideshow/" + slides[position].image;
+  document.querySelector("#banner p").innerHTML = slides[position].tagLine;
+}
